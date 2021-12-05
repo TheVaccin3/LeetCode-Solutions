c8f2d9f761d3770f0b4acd3aab0dfbc733af5135
Saksham Goyal <sakgoy2001@gmail.com>
2021-12-05 01:24:40 -0500

    Time: 0 ms (100.00%), Space: 6 MB (49.30%) - LeetHub

---
+++ maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string x = to_string(num);
+        bool changed = false;
+        for(int i = 0; i < x.length(); i++){
+            if(x[i] == '6'){
+                x[i] = '9';
+                return stoi(x);
+            }
+        }
+        return stoi(x);
+    }
+};
