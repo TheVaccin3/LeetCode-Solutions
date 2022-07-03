15b2b48f3a3d16147d32e800cd3eda7c5fa07ad3
Saksham Goyal <sakgoy2001@gmail.com>
2022-07-03 00:20:29 -0400

    Time: 2 ms (51.78%), Space: 6.3 MB (60.75%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -3,8 +3,8 @@ public:
     int strStr(string haystack, string needle) {
         if(haystack.size() == 0) return 0;
         auto ret =  haystack.find(needle);
-        if (ret == string::npos) return -1;
-        return ret;
+        return ret == string::npos ? -1 : ret;
+        
         
     }
 };
